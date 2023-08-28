a4a3829f80a11103c1808646959535da78056dd6
Melany Dalama <melinahir.md@gmail.com>
2023-08-28 15:07:44 -0300

    creacion de atributos, constructores y metodos de Persona

--- src/Ejercicio1/Persona.java
+++ src/Ejercicio1/Persona.java
@@ -1,5 +1,78 @@
 package Ejercicio1;
 
+import java.util.Objects;
+
 public class Persona {
+	
+	//Atributos
+	
+	String nombre, apellido, dni;
+	
+	//Constructores
+	
+	public Persona() {
+		this.nombre = "Sin nombre";
+		this.apellido = "Sin apellido";
+		this.dni = "xxxxxxxx";
+	}
+	
+	public Persona(String nombre, String apellido, String dni) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+	}
+
+	//Métodos 
+	
+	@Override
+	public String toString() {
+		
+		return "Nombre: " + nombre + ", apellido: " + apellido + ", DNI: " + dni;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(apellido, dni, nombre);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Persona other = (Persona) obj;
+		return Objects.equals(apellido, other.apellido) && Objects.equals(dni, other.dni)
+				&& Objects.equals(nombre, other.nombre);
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	
+	
+	
 
 }
