bf7a86b70e13143d97d5865e6702698e382022f7
lvitale <lucianovitale00@gmail.com>
2023-08-30 14:52:35 -0300

    Creación del array con bufferReader

--- src/Ejercicio1/Principal.java
+++ src/Ejercicio1/Principal.java
@@ -1,5 +1,10 @@
 package Ejercicio1;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.swing.JOptionPane;
 
 public class Principal {
@@ -15,7 +20,7 @@ public class Principal {
 		{
 			System.out.println("El archivo no existe");
 		}
-		archivo.Escribir_Registros("Guido", "Benetti", "8");
+		
 		System.out.println("\n LISTADO: ");
 		archivo.Leer_Registros();
 
@@ -23,11 +28,26 @@ public class Principal {
 		try {
 			Persona.verificarDniInvalido(dni);
 			System.out.println("El dni ingresado es valido!");
+			
 		}
 		catch (DniInvalido e) {
 			System.out.println("El dni ingresado es invalido, solo ingrese numeros.");
 			e.printStackTrace();
 		}
+		
+		  ArrayList<String> arr = new ArrayList<String>();
+	        try (BufferedReader br = new BufferedReader(new FileReader("Archivos/Personas.txt")))
+	        {
+
+	            String sCurrentLine;
+
+	            while ((sCurrentLine = br.readLine()) != null) {
+	                arr.add(sCurrentLine);
+	            }
+
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        } 
 	}
 
 }
